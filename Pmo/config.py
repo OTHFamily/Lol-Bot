645ad7d9237e98350a42b9f7f05e385db27cfc87
OTHFamily <112629165+OTHFamily@users.noreply.github.com>
2022-11-27 06:39:49 +0700

    Create config.py

---
+++ Pmo/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+OWNER_ID = os.environ.get("OWNER_ID", "")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+CHANNEL_DB = os.environ.get("CHANNEL_DB", "")
