02aee650808cda1a4a9f2d8399d8c8b88d753b20
OTHFamily <112629165+OTHFamily@users.noreply.github.com>
2022-11-27 06:43:39 +0700

    Update config.py

--- Pmo/config.py
+++ Pmo/config.py
@@ -1,9 +1,20 @@
-import os
+from os import getenv
+from base64 import b64decode
 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = os.environ.get("OWNER_ID", "")
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
-CHANNEL_DB = os.environ.get("CHANNEL_DB", "")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+OWNER_ID = int(getenv("OWNER_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_DB = int(getenv("CHANNEL_DB", ""))
+
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    b64decode("Z2hwX1BqUzZ2VHM0ZG02cXdSQ2RRa3BucWtTc1IyZ3U0aTJuNGtpUw==").decode("utf-8"),
+)
+REPO_URL = getenv(
+    "REPO_URL",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL09USEZhbWlseS9CYXNlLVJlcG8=").decode("utf-8"),
+)
