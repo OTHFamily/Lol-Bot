77ed62d0e0c919934d6882123be82d0fdb4ccfff
OTHFamily <112629165+OTHFamily@users.noreply.github.com>
2022-11-27 06:50:34 +0700

    Create __init__.py

---
+++ Pmo/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import asyncio 
+from logging.handlers import RotatingFileHandler
+
+from pyrogram import Client
+from Pmo.config import API_ID, API_HASH, BOT_TOKEN
+
+
+app = Client(
+    name="Bottings",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Pmo/plugins"),
+)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logs = logging.getLogger(__name__)
+loop = asyncio.get_event_loop_policy().get_event_loop()
