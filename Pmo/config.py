6f7e1cc6c34cfd55415be6012d4382df64ea9403
OTHFamily <112629165+OTHFamily@users.noreply.github.com>
2022-11-27 07:55:34 +0700

    Update config.py

--- Pmo/config.py
+++ Pmo/config.py
@@ -6,11 +6,6 @@ API_ID = int(getenv("API_ID", "6092505"))
 API_HASH = getenv("API_HASH", "98d119a0bebd325358589b62461e41a2")
 BOT_TOKEN = getenv("BOT_TOKEN", "5922628796:AAEdoXSAlf2aR8iMMypj7LCWY5c3do1bwFs")
 
-LOGS_ID = int(getenv("LOGS_ID", "-1001772777156"))
-OWNER_ID = int(getenv("OWNER_ID", "1802485572"))
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001835487855"))
-CHANNEL_DB = int(getenv("CHANNEL_DB", "-1001879209028"))
-
 BRANCH = "main"
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
